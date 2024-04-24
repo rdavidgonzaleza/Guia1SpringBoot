5917a986cc9916a83bfb2a4005477b76a0e7846a
rdavidgonzaleza <rdavidgonzaleza@outlook.com>
2024-04-24 00:41:28 -0500

    first commit

---
+++ src/test/java/ruben/EjemploSpring/EjemploSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ruben.EjemploSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
